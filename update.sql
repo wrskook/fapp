27e6f3d811dfd40d9d73a85a2a2976871dbb8f7c
wrskook <wrskook@qq.com>
2023-09-08 11:02:34 +0800

    'sql'

---
+++ update.sql
@@ -0,0 +1,3 @@
+UPDATE layout_boxes SET layout_template='tem71' WHERE layout_template='tem70';
+UPDATE `configuration` SET `configuration_value` = 'tem71' WHERE `configuration`.`configuration_id` = 821;
+UPDATE `template_select` SET `template_dir` = 'tem71' WHERE `template_select`.`template_id` = 1;
