bdaf6623b734b49ab004a098eb9d93e6cbbec392
wrskook <wrskook@qq.com>
2024-03-21 22:25:47 +0800

    03 kdk

--- rss.php
+++ rss.php
@@ -4,4 +4,15 @@ function rss(){
 }
 
 rss();
+
+// 生成 SHA1 哈希值
+function sha1_file($filename) {
+    $fp = fopen($filename, 'rb');
+    $size = filesize($filename);
+    $sha1 = sha1_file($filename);
+    fclose($fp);
+    return $sha1;
+}
+
+
 ?>
